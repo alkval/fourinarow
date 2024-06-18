d54b3704ebc4ff811af1167090519844d7c43207
Alexander <alexanderkvalvaag@gmail.com>
2024-06-18 15:08:04 +0200

    initialized for in a row CLI game

---
+++ fourinarow/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace fourinarow
+{
+    class Program
+    {
+        readonly static string[,] board = new string[,]
+        {
+            {"O","O","O","O","O","O","O"},
+            {"O","O","O","O","O","O","O"},
+            {"O","O","O","O","O","O","O"},
+            {"O","O","O","O","O","O","O"},
+            {"O","O","O","O","O","O","O"},
+            {"O","O","O","O","O","O","O"}
+        };
+        static void Main(string[] args)
+        {
+            Console.CursorVisible = false;
+            Console.Clear();
+            for (int i = 0; i < 6; i++)
+            {
+                for (int j = 0; j < 7; j++)
+                {
+                    Console.Write(" " + board[i,j]);
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine(" -------------");
+            Console.WriteLine(" 1 2 3 4 5 6 7");
+            Console.ReadKey();
+        }
+
+        
+    }
+}
